f9c0f5bc0feca0c887deeb41da0016e9a629fdae
edemdev7 <ekpomachi@gmail.com>
2025-04-16 17:22:28 +0200

    Add email field to CustomUser model and update migration

--- users/models.py
+++ users/models.py
@@ -9,9 +9,14 @@ class CustomUser(AbstractUser):
         ('recycleur', 'Recycleur'),
         ('admin', 'Administrateur'),
     ]
+    
+    email = models.EmailField(unique=True)
     type = models.CharField(max_length=20, choices=USER_TYPES, default='particulier')
     location = models.CharField(max_length=255, blank=True, null=True)
     points = models.IntegerField(default=0)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
 
     def __str__(self):
         return f"{self.username} ({self.type})"
