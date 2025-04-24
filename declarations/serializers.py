efaad40cf08216e8055a620157cdbad9be3cd224
edemdev7 <ekpomachi@gmail.com>
2025-04-24 12:53:37 +0200

    Add user and collector username fields to WasteDeclarationSerializer

--- declarations/serializers.py
+++ declarations/serializers.py
@@ -1,13 +1,24 @@
 from rest_framework import serializers
 from .models import WasteDeclaration
+from users.models import CustomUser
 
 class WasteDeclarationSerializer(serializers.ModelSerializer):
+    user_username = serializers.SerializerMethodField()
+    collector_username = serializers.SerializerMethodField()
+    
     class Meta:
         model = WasteDeclaration
-        fields = '__all__'
+        fields = ['id', 'user', 'user_username', 'category', 'weight', 'photo', 
+                  'location', 'status', 'created_at', 'collector', 'collector_username']
         read_only_fields = ['user', 'created_at']
         extra_kwargs = {
             'photo': {'required': False},
             'location': {'required': True},
             'collector': {'required': False},
         }
+    
+    def get_user_username(self, obj):
+        return obj.user.username if obj.user else None
+    
+    def get_collector_username(self, obj):
+        return obj.collector.username if obj.collector else None
