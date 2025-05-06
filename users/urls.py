d522e81c17336651a00a61e9f9ea0c859d2f62df
edemdev7 <ekpomachi@gmail.com>
2025-05-06 05:38:27 +0200

    Add CollectorSchedule model and location_gps field to CustomUser; update URL routing for user profile and collector schedules

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,10 @@
-from django.urls import path
-from .views import AdminProfessionalVerificationListView, AdminValidateProfessionalView, SubmitProfessionalVerificationView, UploadVerificationDocumentsView,UserVerificationListView, ValidateUserView,CollectorListView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import AdminProfessionalVerificationListView, AdminValidateProfessionalView, SubmitProfessionalVerificationView, UploadVerificationDocumentsView,UserVerificationListView, ValidateUserView,CollectorListView, CollectorScheduleViewSet, UserProfileUpdateView
+
+
+router = DefaultRouter()
+router.register(r'collector-schedules', CollectorScheduleViewSet, basename='collector-schedules')
 
 urlpatterns = [
     path('upload-documents/', UploadVerificationDocumentsView.as_view()),
@@ -9,4 +14,8 @@ urlpatterns = [
     path('admin/verifications/', AdminProfessionalVerificationListView.as_view()),
     path('admin/validate-professional/<int:verification_id>/', AdminValidateProfessionalView.as_view()),
     path('collectors/', CollectorListView.as_view(), name='collector-list'),
+    path('profile/update/', UserProfileUpdateView.as_view(), name='profile-update'),
+    path('', include(router.urls)),
+
 ]
+# This file defines the URL routing for the users app in a Django project.
